4d75cb9510083f61ac892d0e68abfa3aaf9e4382
Suyash.Muley <suyash.muley@elastikteams.com>
2025-03-24 18:33:31 +0530

    connection string test

--- tap_aptify/client.py
+++ tap_aptify/client.py
@@ -11,7 +11,7 @@ from uuid import uuid4
 from typing import Any, Iterable, Iterator
 
 import pendulum
-# import pyodbc
+import pyodbc
 import sqlalchemy
 
 from sqlalchemy.engine import Engine
@@ -32,8 +32,8 @@ class aptifyConnector(SQLConnector):
         """Class Default Init"""
         # If pyodbc given set pyodbc.pooling to False
         # This allows SQLA to manage to connection pool
-        # if config.get('driver_type') == 'pyodbc':
-        #     pyodbc.pooling = False
+        if config.get('driver_type') == 'pyodbc':
+            pyodbc.pooling = False
 
         super().__init__(config, sqlalchemy_url)
 
@@ -46,6 +46,10 @@ class aptifyConnector(SQLConnector):
         Returns:
             The URL as a string.
         """
+         # If a connection string is provided, use it directly.
+        if 'connection_string' in config:
+            return config['connection_string']
+        
         url_drivername = f"{config.get('dialect', 'mssql')}+{config.get('driver_type', 'pyodbc')}"
         
         config_url = URL.create(
